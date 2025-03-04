a6d72a083078e434587a9d5e700c56ba984e9c9b
Shady Khalifa <dev+github@shadykhalifa.me>
2025-03-04 22:18:56 +0200

    refactor: remove python code

--- lionzhd/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import XtreamConfig, FavoriteItem
-
-# Register your models here.
-
-admin.site.register(XtreamConfig)
-admin.site.register(FavoriteItem)

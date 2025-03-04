0916e31d82067bbd81f17eb10fffd836d6ecea6d
Shady Khalifa <dev+github@shadykhalifa.me>
2025-03-04 23:58:41 +0200

    feat: add laravel react starter-kit

--- flake.nix
+++ flake.nix
@@ -6,29 +6,81 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        pythonPackages = pkgs.python3Packages;
+        stdenv = pkgs.stdenv;
+        myphp = pkgs.php84.override {
+          embedSupport = true;
+          ztsSupport = true;
+          staticSupport = stdenv.hostPlatform.isDarwin;
+          zendSignalsSupport = false;
+          zendMaxExecutionTimersSupport = stdenv.hostPlatform.isLinux;
+        };
+        php = myphp.buildEnv {
+          extensions = (
+            { enabled, all }:
+            enabled
+            ++ (with all; [
+              # Add More PHP Extensions Here
+              xdebug
+              gd
+              redis
+              pdo
+              pdo_mysql
+              gmp
+              imagick
+              bcmath
+              opcache
+              intl
+              memcached
+              sockets
+              pcntl
+              zip
+              zstd
+              apcu
+            ])
+          );
+          extraConfig = ''
+            date.timezone = "UTC"
+            # turn off xdebug by default
+            xdebug.mode=off
+            #xdebug.mode=debug,develop
+            #xdebug.start_with_request=yes
+            #xdebug.start_upon_error=yes
+            #xdebug.idekey=VSCODE
+          '';
+        };
+        frankenphp = pkgs.frankenphp.override {
+          inherit php;
+        };
       in
       {
         devShells.default = pkgs.mkShell {
-          name = "lionz";
-          venvDir = "./.venv";
-          nativeBuildInputs = [
-            pkgs.pkg-config
-          ];
+          name = "lionzhd";
+          nativeBuildInputs = [ ];
           buildInputs = [
-            pythonPackages.venvShellHook
-            pythonPackages.python
-            pkgs.uv
+            # Nodejs
+            pkgs.nodePackages.typescript-language-server
+            pkgs.nodejs_22
+            pkgs.pnpm
+
+            php
+            frankenphp
+            php.packages.composer
+            php.packages.php-cs-fixer
+            php.packages.phpstan
+            pkgs.blade-formatter
+            pkgs.phpactor
           ];
-          packages = [
-            pkgs.meilisearch
-          ];
+          packages = [ ];
+
+          COMPOSER_HOME = ".composer";
         };
-      });
+      }
+    );
 }

7d7497533c328424e9acd9f9743d5510f04dc542
Shady Khalifa <dev+github@shadykhalifa.me>
2025-04-04 01:29:26 +0200

    chore: update templates

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="og:site_name" content="{{ config('app.name') }}">
+        <meta name="og:title" content="{{ config('app.name') }}">
+        <meta name="og:description" content="Simple and fast web application">
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
             (function() {
@@ -19,17 +22,17 @@
         {{-- Inline style to set the HTML background color based on our theme in app.css --}}
         <style>
             html {
-                background-color: oklch(1 0 0);
+                background-color: var(--color-background);
             }
             html.dark {
-                background-color: oklch(0.145 0 0);
+                background-color: var(--color-background-dark);
             }
         </style>
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=inter:400,500,600" rel="stylesheet" />
 
         @routes
         @viteReactRefresh

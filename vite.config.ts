3c04d0ea58b78e869e559da2512e2c313ebe9efa
Shady Khalifa <dev+github@shadykhalifa.me>
2026-02-27 00:22:37 +0000

    fix(05): orchestrator corrections

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        host: '127.0.0.1',
+        hmr: {
+            host: '127.0.0.1',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
